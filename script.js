9059a36755ac6b2ac0e14a449dbc555aea5d94e4
steniowrsilva <steniowoney@gmail.com>
2023-02-19 00:00:43 -0300

    add last styles in css file

--- script.js
+++ script.js
@@ -1,65 +1,106 @@
-
 let container = document.querySelector('#container');
-let numberOfSquares = 100;  //  heightAndWidth = 20;  numberOfSquares = (200/heightAndWidth)**2;
-
-function createSquares () {
-    for (let i=0; i<numberOfSquares; i++) {
+let numberOfSquaresPerSide = 16;  //  heightAndWidth = 20;  numberOfSquares = (320/heightAndWidth)**2;
+function createSquares (numberOfSquaresPerSide) {
+    for (let i=0; i<numberOfSquaresPerSide**2; i++) {
         let square = document.createElement('div');
         square.classList.add('square');
         container.appendChild(square);
+
+        square.addEventListener('mouseover', (event)=>{
+            if (square.classList.contains('rgb')) {
+                let r = Math.floor(Math.random()*255);
+                let g = Math.floor(Math.random()*255);
+                let b = Math.floor(Math.random()*255);
+                event.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+            } else if(square.classList.contains('grey')){
+                let startingGreyRGB = 255 / 2;
+                if (square.classList.contains('blackSquare')){
+                    event.target.classList.remove('blackSquare');
+                    event.target.style.backgroundColor = startingGreyRGB;
+                }
+                if (square.style.backgroundColor) {
+                    let currentColor = square.style.backgroundColor;
+                    let matches = currentColor.match(/\d+/g);
+                    startingGreyRGB = Math.max(...matches); // use the maximum RGB value as the starting grey value
+                }
+                let newGreyRGB = Math.floor(startingGreyRGB-0.1*(225/2)); // decrease by 10% of rgb(255/2,255/2,255/2)
+                event.target.style.backgroundColor = `rgb(${newGreyRGB},${newGreyRGB},${newGreyRGB})`;
+            } else if (square.classList.contains('black')) {
+                // square.style.backgroundColor = 'black';
+                event.target.classList.add('blackSquare');
+            } else {
+                // square.style.backgroundColor = 'black';
+                event.target.classList.add('blackSquare');
+            }
+        });
     }
+    let input = document.querySelector('input')
+    let valuesText = document.querySelector('#valuesText');
+    valuesText.textContent = input.value;
+
 }
 
-createSquares();
+createSquares(numberOfSquaresPerSide);
 
-const buttonFive = document.querySelector('#five');
-buttonFive.addEventListener("click", () => {
+const chooseGrid = document.querySelector('#chooseGrid');
+chooseGrid.addEventListener("click", ()=>{
+    numberOfSquaresPerSide = prompt('Number of squares per side:');
+    while (numberOfSquaresPerSide>100){
+        numberOfSquaresPerSide = prompt('It cannot be greater than 100. Number of squares per side:');
+    }
+    if (!numberOfSquaresPerSide){
+        numberOfSquaresPerSide = 16;
+    }
     container.innerHTML = '';
-    numberOfSquares = 1600;
-    createSquares();
+    createSquares(numberOfSquaresPerSide);
     let squares = document.querySelectorAll('.square');
+    heightAndWidth = (640/numberOfSquaresPerSide) - 2;
     squares.forEach(square => {
-        square.style.height = '3px';
-        square.style.width = '3px';
+        square.style.height = `${heightAndWidth}px`; 
+        square.style.width = `${heightAndWidth}px`;
     })
-    
 })
 
-const buttonTen = document.querySelector('#ten');
-buttonTen.addEventListener("click", () => {
+let gridvalues = document.querySelector('input');
+let valuesText = document.querySelector('#valuesText');
+gridvalues.addEventListener('input', ()=>{
+    numberOfSquaresPerSide = gridvalues.value;
+    valuesText.textContent = gridvalues.value;
     container.innerHTML = '';
-    numberOfSquares = 400;
-    createSquares();
+    createSquares(numberOfSquaresPerSide);
     let squares = document.querySelectorAll('.square');
+    heightAndWidth = (640/numberOfSquaresPerSide) - 2;
     squares.forEach(square => {
-        square.style.height = '8px';
-        square.style.width = '8px';
+        square.style.height = `${heightAndWidth}px`; 
+        square.style.width = `${heightAndWidth}px`;
     })
-    
 })
 
-const buttonTwenty =  document.querySelector('#twenty');
-buttonTwenty.addEventListener("click", () => {
-    container.innerHTML = '';
-    numberOfSquares = 100;
-    createSquares();
+const rgbButton = document.querySelector('#rgbButton');
+rgbButton.addEventListener('click', () => {
     let squares = document.querySelectorAll('.square');
     squares.forEach(square => {
-        square.style.height = '18px';
-        square.style.width = '18px';
-    })
-    
+        square.classList.add('rgb');
+    });
 })
 
-const buttonForty = document.querySelector('#forty');
-buttonForty.addEventListener("click", () => {
-    container.innerHTML = '';
-    numberOfSquares = 25;
-    createSquares();
+const greyButton = document.querySelector('#greyButton');
+
+greyButton.addEventListener("click", () => {
     let squares = document.querySelectorAll('.square');
     squares.forEach(square => {
-        square.style.height = '38px';
-        square.style.width = '38px';
-    })
-    
+        square.classList.remove('rgb');
+        square.classList.add('grey');
+    });
 })
+
+let blackButton = document.querySelector('#blackButton')
+blackButton.addEventListener("click", ()=>{
+    let squares = document.querySelectorAll('.square');
+    squares.forEach(square => {
+        square.classList.remove('rgb');
+        square.classList.remove('grey');
+        square.classList.add('black');
+    });
+})
+
